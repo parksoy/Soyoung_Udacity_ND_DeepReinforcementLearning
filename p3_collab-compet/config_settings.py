dcd31b1d77e5892fd4934739d83dfa35d1913186
parksoy <parksoy@users.noreply.github.com>
2019-05-11 23:08:19 -0700

    attempt 5-9 local

--- p3_collab-compet/config_settings.py
+++ p3_collab-compet/config_settings.py
@@ -1,11 +1,11 @@
 import torch
 
 class Args:
-    num_episodes=2          # Num of games/episode
+    num_episodes=3000          # Num of games/episode
     max_steps=1000             # max num of times of steps in one episode
     batch_size=1024            # how many experiences are sampled and learned in batch processing to go through neural networks
 
-    actor_learn_rate=0.0001    # actors learning rate
+    actor_learn_rate=0.001    # actors learning rate
     critic_learn_rate=0.001    # critic learning rate
 
     update_every=20            # frequency to learn with stability
@@ -17,10 +17,12 @@ class Args:
     noise_sigma=0.2            # noise spread
     noise_factor_decay = 1e-6  # Noise decay speed
 
-    layer_sizes=[128,128]      # Neural Network unit size for first, second layers. ,128
+    layer_sizes=[512,512]      # Neural Network unit size for first, second layers. ,128
 
     buffer_size=3000000        # replay buffer size
     noise_factor=1
     print_every=100            # stdout print frequency
+
+    scoreGoal=0.001
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")   #automatically determine 'gpu' or 'cpu'
--- p3_collab-compet/running_gpu_model.py
+++ p3_collab-compet/running_gpu_model.py
@@ -4,36 +4,41 @@ import torch.nn as nn
 import torch.nn.functional as F
 from config_settings import Args
 
-# %load_ext autoreload
-# %autoreload 2
-
 args=Args()
+device = args.device
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print("device=",device)
+
 
 def hidden_init(layer):
     fan_in = layer.weight.data.size()[0]
     lim = 1. / np.sqrt(fan_in)
     return (-lim, lim)
 
+
 class Actor(nn.Module):
     """Actor (Policy) Model."""
-    def __init__(self, state_size, action_size, seed, fc1_units=args.layer_sizes[0], fc2_units=args.layer_sizes[1]): #States[0] (33,)
+
+    def __init__(self, state_size, action_size, seed,
+                 fc1_units=args.layer_sizes[0],
+                 fc2_units=args.layer_sizes[1]):
         """Initialize parameters and build model.
+        Params
+        ======
             state_size (int): Dimension of each state
             action_size (int): Dimension of each action
             seed (int): Random seed
             fc1_units (int): Number of nodes in first hidden layer
-            fc2_units (int): Number of nodes in second hidden layer """
+            fc2_units (int): Number of nodes in second hidden layer
+        """
         super(Actor, self).__init__()
         self.seed = torch.manual_seed(seed)
 
-        #self.bn0 = nn.BatchNorm1d(state_size).to(device)
-        self.fc1 = nn.Linear(state_size, fc1_units).to(device) #33
-        #self.bn1 = nn.BatchNorm1d(fc1_units).to(device)
-        self.fc2 = nn.Linear(fc1_units, fc2_units).to(device)
-        #self.bn2 = nn.BatchNorm1d(fc2_units).to(device)
-        self.fc3 = nn.Linear(fc2_units, action_size).to(device) #4
+        self.bn0 = nn.BatchNorm1d(state_size)
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.bn1 = nn.BatchNorm1d(fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.bn2 = nn.BatchNorm1d(fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -43,30 +48,31 @@ class Actor(nn.Module):
 
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
-        if str(type(state))=="<class \'numpy.ndarray\'>":
-            states = torch.from_numpy(states).float().to(device)
-
-        #x = self.bn0(state).to(device)
-        x = F.relu(self.fc1(state)) #x = F.relu(self.bn1(self.fc1(x))) #
-        x = F.relu(self.fc2(x)) #x = F.relu(self.bn2(self.fc2(x)))
-        return F.tanh(self.fc3(x))
+        x = self.bn0(state)
+        x = F.relu(self.bn1(self.fc1(x)))
+        x = F.relu(self.bn2(self.fc2(x)))
+        return torch.tanh(self.fc3(x))
 
 class Critic(nn.Module):
     """Critic (Value) Model."""
-    def __init__(self, state_size, action_size, seed, fcs1_units=args.layer_sizes[0], fc2_units=args.layer_sizes[1]):
+
+    def __init__(self, state_size, action_size, seed, fcs1_units=512, fc2_units=512):
         """Initialize parameters and build model.
         Params
+        ======
             state_size (int): Dimension of each state
             action_size (int): Dimension of each action
             seed (int): Random seed
             fcs1_units (int): Number of nodes in the first hidden layer
-            fc2_units (int): Number of nodes in the second hidden layer """
+            fc2_units (int): Number of nodes in the second hidden layer
+            fc3_units (int): Number of nodes in the third hidden layer
+        """
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
-        #self.bn0 = nn.BatchNorm1d(state_size).to(device)
-        self.fcs1 = nn.Linear(state_size, fcs1_units).to(device)
-        self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units).to(device)
-        self.fc3 = nn.Linear(fc2_units, 1).to(device)
+        self.bn0 = nn.BatchNorm1d(state_size)
+        self.fcs1 = nn.Linear(state_size, fcs1_units)
+        self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, 1)
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -76,10 +82,7 @@ class Critic(nn.Module):
 
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
-
-        if str(type(state))=="<class \'numpy.ndarray\'>":
-            state = torch.from_numpy(state).float().to(device)
-        #state = self.bn0(state)
+        state = self.bn0(state)
         xs = F.relu(self.fcs1(state))
         x = torch.cat((xs, action), dim=1)
         x = F.relu(self.fc2(x))
